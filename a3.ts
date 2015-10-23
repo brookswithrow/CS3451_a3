5ad5ae9e7e72e8872d27e4874388756a724727f9
Brooks Withrow <bwithrow3@gatech.edu>
2015-10-23 15:08:12 -0400

    Solution Implementation
    I’m bad at github

--- a3.ts
+++ a3.ts
@@ -86,21 +86,157 @@ var onLoad = function (mesh: loader.Mesh) {
   var normal = [];
   // this is where you put the triangle vertex list
   var indices = [];
+
+  //Push values from mesh vertex array onto position, position's done
+  for (var i = 0; i < numVerts; i++) {
+    for (var j = 0; j < 3; j++) {
+      position.push(mesh.v[i][j]);
+    }
+  }
+  
+  //Push values from mesh triangle array onto indices, indices is done
+ for (var i = 0; i < numTris; i++) {
+    for (var j = 0; j < 3; j++) {
+      indices.push(mesh.t[i][j]);
+    }
+  }
+  
+  //Push 3 random colors and 255 (alpha) for each vertex
+ for (var i = 0; i < numVerts; i++) {
+    for (var j = 0; j < 3; j++) {
+      var c = rand(0, 255)
+      color.push(c);
+    }
+    color.push(255);
+  }
+  
+  //Vertex normals y'all
+  
+  //The triangle normals
+  var tn = [];
+  //The vertex normals
+  var vn = [];
+  
+  //Setting up our 2D arrays
+  for (var i = 0; i < numTris; i++) {
+    tn.push([]); 
+  }
+  for (var i = 0; i < numVerts; i++) {
+    vn.push([]);
+  }
+  
+    for (var i = 0; i < vn.length; i++) {
+    vn[i].push(0);
+    vn[i].push(0);
+    vn[i].push(0);
+  }
+  
+  //Getting triangle normals, by normalizing ab x ac
+  for (var i = 0; i < numTris; i++) {
+    //Our vertexes
+    var a = mesh.t[i][0];
+    var b = mesh.t[i][1];
+    var c = mesh.t[i][2];
     
-  //////////////
-  ///////// YOUR CODE HERE TO TAKE THE MESH OBJECT AND CREATE ALL THE INFORMATION NEEDED TO RENDER
-  //////////////
+    //Getting ab & ac
+    var bx = mesh.v[b][0] - mesh.v[a][0];
+    var by = mesh.v[b][1] - mesh.v[a][1];
+    var bz = mesh.v[b][2] - mesh.v[a][2];
+        
+    var cx = mesh.v[c][0] - mesh.v[a][0];
+    var cy = mesh.v[c][1] - mesh.v[a][1];
+    var cz = mesh.v[c][2] - mesh.v[a][2];
+  
+    //Cross product
+    var x = by*cz - cy*bz;
+    var y = bz*cx - cz*bx;
+    var z = bx*cy - cx*by;
+    
+    //Normalizing
+    var l = Math.sqrt(x*x + y*y + z*z);
+    x /= l;
+    y /= l;
+    z /= l;
+    
+    //Add to triangle normals array
+    tn[i].push(x);
+    tn[i].push(y);
+    tn[i].push(z);
+  }
+  
+  //Going through the triangle normals and adding them to the vertex normal of the triangle's vertices
+  for (i = 0; i < numTris; i++) {
+    //Vertices of triangle i
+    var a = mesh.t[i][0];
+    var b = mesh.t[i][1];
+    var c = mesh.t[i][2];
+    
+    vn[a][0] += tn[i][0];
+    vn[a][1] += tn[i][1];
+    vn[a][2] += tn[i][2];
+    
+    vn[b][0] += tn[i][0];
+    vn[b][1] += tn[i][1];
+    vn[b][2] += tn[i][2];
+    
+    vn[c][0] += tn[i][0];
+    vn[c][1] += tn[i][1];
+    vn[c][2] += tn[i][2];
+  }
+  
+  //Normalizing our vertex normals
+  for (i = 0; i < vn.length; i++) {
+    var normx = vn[i][0];
+    var normy = vn[i][1];
+    var normz = vn[i][2];
+    
+    var l = Math.sqrt(normx*normx + normy*normy + normz*normz);
+    vn[i][0] = normx/l;
+    vn[i][1] = normy/l;
+    vn[i][2] = normz/l;
+  }
+  
+  //Push the vertex normals onto normal
+  for (i = 0; i < numVerts; i++) {
+    for (j = 0; j < 3; j++) {
+      normal.push(vn[i][j]);
+    }
+  }
   
   // bb1 and bb2 are the corners of the bounding box of the object.  
   var bb1 = vec3.create();
   var bb2 = vec3.create();
   
+  bb1[0] = mesh.v[0][0];
+  bb1[1] = mesh.v[0][1];
+  bb1[2] = mesh.v[0][2];
+  
+  bb2[0] = mesh.v[0][0];
+  bb2[1] = mesh.v[0][1];
+  bb2[2] = mesh.v[0][2];
+  //Loop through, make sure bb1 has the smallest coordinates and bb2 has the largest coordinates
+  //out of all vertex coordinates
+  for (i = 0; i < numVerts; i++) {
+    bb1[0] = Math.min(bb1[0], mesh.v[i][0]);
+    bb1[1] = Math.min(bb1[1], mesh.v[i][1]);
+    bb1[2] = Math.min(bb1[2], mesh.v[i][2]);
+    
+    bb2[0] = Math.max(bb2[0], mesh.v[i][0]);
+    bb2[1] = Math.max(bb2[1], mesh.v[i][1]);
+    bb2[2] = Math.max(bb2[2], mesh.v[i][2]);
+  }
+  
+  //Get the center as the midpoint of bb1->bb2
+  var cx = (bb2[0] + bb1[0])/2;
+  var cy = (bb2[1] + bb1[1])/2;
+  var cz = (bb2[2] + bb1[2])/2;
+  
   // Setup the new object.  you can add more data to this object if you like
   // to help with subdivision (for example)
   newObject = {
     boundingBox: [bb1, bb2],
-    scaleFactor: 1,  // FIX!  the scale should be such that the largest view of the object is 300 units
-    center: [0, 0, 0],  // FIX!  the center of the object
+    scaleFactor: 300/vec3.distance(bb1,bb2),
+    center: [cx, cy, cz],  
     numElements: indices.length,
     arrays: {
       position: new Float32Array(position),
@@ -141,13 +277,250 @@ window["loadModel"] = () => {
     // attempt to download the modele
     loader.loadMesh("models/" + filename.value, onLoad, onProgress, onError);
 }
- 
+
 window["onSubdivide"] = () => {
-    console.log("Subdivide called!  You should do the subdivision!");
+  //Corner operators
+  function t(c: number): number {
+    return Math.floor(c/3)
+  }
+  
+  function n(c: number): number {
+    return 3*t(c)+(c+1)%3;
+  }
+  
+  function p(c: number): number {
+    return n(n(c));
+  }
+  
+  function b(c: number, o: Array<number>): boolean {
+    return o[c] == c;
+  }
+  
+  function l(c: number, o: Array<number>): number {
+    return o[n(c)];
+  }
+  
+  function r(c: number, o: Array<number>): number {
+    return o[p(c)];
+  }
+  
+  //Midpoint function for bulge 
+  function midpoint(ax: number, ay: number, az: number, bx:number, by:number, bz:number): Array<number> {
+    var ret = [];
+    ret[0] = (ax + bx)/2;
+    ret[1] = (ay+by)/2;
+    ret[2] = (az+bz)/2;
+    return ret;
+  }
+  //New arrays for the newObject
+    var position = [];
+    var normal = [];
+    var color = [];
+    var indices = [];
     
-  //////////////
-  ///////// YOUR CODE HERE TO TAKE THE CURRENT OBJECT and SUBDIVIDE it, creating a newObject
-  //////////////
+    //Move all the positions to a new array
+    for (var i = 0; i < object.arrays.position.length; i++) {
+      position.push(object.arrays.position[i]);
+    }
+    //Move all the indices to a new array
+     for (var i = 0; i < object.arrays.indices.length; i++) {
+      indices.push(object.arrays.indices[i]);
+    }
+    //Set up the o table. If a corner has no opposite, o[i] = i, so initialize values as such
+    var o = [];
+    for (var i = 0; i < object.arrays.indices.length; i++) {
+      o[i] = i;
+    }
+    //Filling in the o table
+    for (var i = 0; i < object.arrays.indices.length; i++) {
+      for (var j = i + 1; j < object.arrays.indices.length; j++) {
+          if (indices[n(i)] == indices[p(j)] && indices[p(i)] == indices[n(j)]) {
+            o[i] = j;
+            o[j] = i;
+          }
+      }
+    }
+    //Subdivision, using pseudocode from slides
+    var w = [];
+    var nv = position.length / 3;
+    var nc = indices.length;
+    var nt = nc/3;
+    //splitEdges
+    for (var i = 0; i < nc; i++) {
+      if (b(i, o)) {
+        var pr = 3*indices[p(i)];
+        var nx = 3*indices[n(i)];
+        var mx = (position[pr] + position[nx])/2;
+        var my = (position[pr+1] + position[nx+1])/2;
+        var mz = (position[pr+2] + position[nx+2])/2;
+        position.push(mx);
+        position.push(my);
+        position.push(mz)
+        w[i] = nv++;
+      }
+      else if (i < o[i]) {
+        var pr = 3*indices[p(i)];
+        var nx = 3*indices[n(i)];
+        var mx = (position[pr] + position[nx])/2;
+        var my = (position[pr+1] + position[nx+1])/2;
+        var mz = (position[pr+2] + position[nx+2])/2;
+        position.push(mx);
+        position.push(my);
+        position.push(mz);
+        w[o[i]] = nv;
+        w[i] = nv++;
+      }
+    }
+    //bulge
+    for (var i = 0; i < nc; i++) {
+      if (!b(i, o) && (i < o[i])) {
+        if (!b(p(i),o) && !b(n(i),o) && !b(p(o[i]),o) && !b(n(o[i]), o)) {
+          var ri = 3*indices[r(i, o)];
+          var li = 3*indices[l(i, o)];
+          var roi = 3*indices[r(o[i], o)];
+          var loi = 3*indices[l(o[i], o)];
+          var ii = 3*indices[i]
+          var oi = 3*indices[o[i]];
+          var ovec = midpoint(position[roi], position[roi+1], position[roi+2], position[loi], position[loi+1], position[loi+2]);
+          var ivec = midpoint(position[ri], position[ri+1], position[ri+2], position[li], position[li+1], position[li+2]);
+          var iomp = midpoint(position[ii], position[ii+1], position[ii+2], position[oi], position[oi+1], position[oi+2]);
+          var midp = midpoint(ovec[0], ovec[1], ovec[2], ivec[0], ivec[1], ivec[2]);
+          var resx = 0.25*(iomp[0] - midp[0]);
+          var resy = 0.25*(iomp[1] - midp[1]);
+          var resz = 0.25*(iomp[2] - midp[2]);
+          position[3*w[i]] += resx;
+          position[3*w[i]+1] += resy;
+          position[3*w[i]+2] += resz;
+        }
+      }
+    }
+    //splitTriangles
+    for (var i = 0; i < 3*nt; i += 3) {
+      indices[3*nt+i] = indices[i];
+      indices[n(3*nt+i)] = w[p(i)];
+      indices[p(3*nt+i)] = w[n(i)];
+      
+      indices[6*nt+i] = indices[n(i)];
+      indices[n(6*nt+i)] = w[i];
+      indices[p(6*nt+i)] = w[p(i)];
+      
+      indices[9*nt+i] = indices[p(i)];
+      indices[n(9*nt+i)] = w[n(i)];
+      indices[p(9*nt+i)] = w[i];
+      
+      indices[i] = w[i];
+      indices[n(i)] = w[n(i)];
+      indices[p(i)] = w[p(i)];
+    }
+      nt *= 4;
+      nc = 3*nt; 
+  
+  //Computing normals, round 2. Same as above, but slightly edited to use
+  //indices & position arrays instead of vertex & triangle arrays  
+  var tn = [];
+  var vn = [];
+  
+  for (var i = 0; i < nt; i++) {
+    tn.push([]); 
+  }
+  for (var i = 0; i < nv; i++) {
+    vn.push([]);
+  }
+  
+  for (var i = 0; i < vn.length; i++) {
+    vn[i].push(0);
+    vn[i].push(0);
+    vn[i].push(0);
+  }
+
+  for (var i = 0; i < nt; i++) {
+    var ai = indices[3*i];
+    var bi = indices[3*i+1];
+    var ci = indices[3*i+2];
+    
+    ai *= 3;
+    bi *= 3;
+    ci *= 3;
+    
+    var bx = position[bi] - position[ai];
+    var by = position[bi + 1] - position[ai + 1];
+    var bz = position[bi + 2] - position[ai + 2];
+        
+    var cx = position[ci] - position[ai];
+    var cy = position[ci + 1] - position[ai + 1];
+    var cz = position[ci + 2] - position[ai + 2];
+  
+    var x = by*cz - cy*bz;
+    var y = bz*cx - cz*bx;
+    var z = bx*cy - cx*by;
+    
+    var len = Math.sqrt(x*x + y*y + z*z);
+    x /= len;
+    y /= len;
+    z /= len;
+    
+    tn[i].push(x);
+    tn[i].push(y);
+    tn[i].push(z);
+  }
+  
+  for (i = 0; i < nt; i++) {
+    var ai = indices[3*i];
+    var bi = indices[3*i+1];
+    var ci = indices[3*i+2];
+    
+    vn[ai][0] += tn[i][0];
+    vn[ai][1] += tn[i][1];
+    vn[ai][2] += tn[i][2];
+    
+    vn[bi][0] += tn[i][0];
+    vn[bi][1] += tn[i][1];
+    vn[bi][2] += tn[i][2];
+    
+    vn[ci][0] += tn[i][0];
+    vn[ci][1] += tn[i][1];
+    vn[ci][2] += tn[i][2];
+  }
+  
+  for (i = 0; i < vn.length; i++) {
+    var normx = vn[i][0];
+    var normy = vn[i][1];
+    var normz = vn[i][2];
+    
+    var len = Math.sqrt(normx*normx + normy*normy + normz*normz);
+    vn[i][0] = normx/len;
+    vn[i][1] = normy/len;
+    vn[i][2] = normz/len;
+  }
+
+  //Put vertex normals in normal array
+  for (i = 0; i < vn.length; i++) {
+    for (j = 0; j < 3; j++) {
+      normal.push(vn[i][j]);
+    }
+  }
+  
+  //Make new colors
+  for (var i = 0; i < nv; i++) {
+      color.push(rand(0, 255));
+      color.push(rand(0, 255));
+      color.push(rand(0, 255));
+      color.push(255);
+    }
+  
+  //Make the newObject
+    newObject = {
+    boundingBox: object.boundingBox,
+    scaleFactor: object.scaleFactor,
+    center: object.center,  
+    numElements: nc,
+    arrays: {
+      position: new Float32Array(position),
+      normal: new Float32Array(normal),
+      color: new Uint8Array(color),
+      indices: new Uint16Array(indices)
+    }
+  };
 } 
 
 ////////////////////////////////////////////////////////////////////////////////////////////
